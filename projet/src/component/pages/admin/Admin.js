bf9d21f5892d9f41f3b5073cbeeccb08afdaf8e8
farah <farahraboudi9999@gmail.com>
2023-07-25 21:49:10 +0200

    first commit

---
+++ projet/src/component/pages/admin/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebaradmin from './Sidebaradmin'
+import Topbaradmin from './Topbaradmin'
+export default function Admin() {
+  return (
+    <div>
+    <Topbaradmin className="container"/>
+      <Sidebaradmin/>
+      
+    </div>
+  )
+}
